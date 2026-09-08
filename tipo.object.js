7b599a2685b9fa0aae82857fb003541a7b327bae
mariaeduardaferretti958-ctrl <mariaeduardaferretti958@gmail.com>
2026-09-08 17:02:31 -0400

    add-object

---
+++ tipo.object.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "ferretti",
+    idade: 28, 
+    casado: true,
+};
+//acessando propriedades do objeto
+console.log(pessoa.nome); //"ferretti"
+console.log(pessoa.idade); // 28
