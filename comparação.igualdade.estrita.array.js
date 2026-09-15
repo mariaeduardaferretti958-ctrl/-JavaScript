43191ff16cf34ed930e390d44ec322d06ba74b39
joao gabriel <mariaeduardaferretti958@gmail.com>
2026-09-15 14:06:20 -0400

    add-comparação.igualdade.estrita.array.js

---
+++ comparação.igualdade.estrita.array.js
@@ -0,0 +1,6 @@
+// listaA é uma array com 5 elementos 
+let listaA = [10, 20, 30, 40, 50]
+// lestaB é outro array com os mesmos 5 elementos que listaA
+let listaB = [10, 20, 30, 40, 50]
+
+console.log(listaA === listaB);
